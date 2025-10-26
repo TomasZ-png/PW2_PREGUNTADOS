95fa856ea589c4a71222042c3d83953c2ff3ab07
TomasZ-png <animacionesc4d@gmail.com>
2025-10-26 20:47:40 -0300

    tablas agregadas

--- core/preguntados_db.sql
+++ core/preguntados_db.sql
@@ -17,3 +17,34 @@ CREATE TABLE usuario(
 
 INSERT INTO usuario (nombre_completo, correo, password, anio_nacimiento, sexo, foto_perfil, rol)
 VALUES ('admin', 'admin@test.com', '$2y$10$IFiN1ghfvGdg2vFHf7.wcethB0wCbUXCDXHAO0XCr4wGEmcrmn/5m', '', '', '', 'ADMIN');
+
+CREATE TABLE partida(
+    id_partida INT AUTO_INCREMENT PRIMARY KEY,
+    resultado VARCHAR(10),
+    puntajeGanado DOUBLE,
+    fecha_creacion DATE
+);
+
+CREATE TABLE categoria_pregunta(
+   id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+   categoria VARCHAR(50)
+);
+
+CREATE TABLE pregunta(
+    id_pregunta INT AUTO_INCREMENT PRIMARY KEY,
+    pregunta VARCHAR(255),
+    dificultad VARCHAR(50),
+    categoria VARCHAR(100),
+    puntaje INT,
+    respuesta_correcta INT,
+    cant_acertadas INT,
+    cant_erroneas INT,
+    fecha_creacion DATE
+);
+
+CREATE TABLE respuesta(
+    id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
+    respuesta VARCHAR(255),
+    id_pregunta INT,
+    FOREIGN KEY (id_pregunta) REFERENCES pregunta(id_pregunta)
+);
