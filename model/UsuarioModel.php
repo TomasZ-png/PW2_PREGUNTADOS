730b3ebf6c7fab29e95e666ab00115b37fab0e07
TomasZ-png <animacionesc4d@gmail.com>
2025-10-27 19:28:17 -0300

    ranking de jugadores

--- model/UsuarioModel.php
+++ model/UsuarioModel.php
@@ -9,6 +9,17 @@ class UsuarioModel
         $this->conexion = $conexion;
     }
 
+    public function listarUsuarios(){
+        $stmt = $this->conexion->prepare("SELECT nombre_completo, foto_perfil, puntaje_global
+                                          FROM usuario 
+                                          WHERE rol = 'USER' 
+                                          ORDER BY puntaje_global DESC");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $usuarios = $result->fetch_all(MYSQLI_ASSOC);
+        return $usuarios;
+    }
+
     public function sumarPuntosUsuario($id_usuario, $puntaje_final){
         $stmt = $this->conexion->prepare("UPDATE usuario 
                                            SET puntaje_global = COALESCE(puntaje_global, 0) + ? 
